6beb41394edba130eae8308c0a7858151253d9ef
drewm1192 <andrewcmcmanus@gmail.com>
2020-10-12 15:52:23 -0400

    Added initializer function
    Set up initializer function so that it triggers the whole app functionality upon startup.

--- app.js
+++ app.js
@@ -53,8 +53,32 @@ var UIController = (function(){
 
  var controller = (function(budgetCtrl,UICtrl){
     
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function(){
+        //Get access to all DOM event listeners.
+        var DOM = UICtrl.getDOMstrings();
+        //Controls the action that occurs when someone clicks the checkmark button
+        document.querySelector(DOM.inputButton).addEventListener("click",ctrlAddItem);
+        /**
+         * We then add an event listener for when someone hits the return
+         * key so that it updates the budget when they hit enter. We don't
+         * put it on a querySelector or specific ID because this action is not 
+         * tied to a specific place on the document. Just if the user hits enter
+         * anywhere on the document, it should add if there is something to add.
+         * The function in this case takes one argument, which is they key 
+         */
+        document.addEventListener("keypress",function(event){
+            //Which is used by older browsers. Use an or statement for 
+            //compatability with other browsers.
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
 
+
+    /**
+     * This function manages adding a new item to the page.
+     */
     var ctrlAddItem = function(){
 
         //1. Get the field input data
@@ -66,33 +90,18 @@ var UIController = (function(){
         console.log(input);
     };
     
-    //Controls the action that occurs when someone clicks the checkmark button
-    document.querySelector(DOM.inputButton).addEventListener("click",ctrlAddItem);
-
-    /**
-     * We then add an event listener for when someone hits the return
-     * key so that it updates the budget when they hit enter. We don't
-     * put it on a querySelector or specific ID because this action is not 
-     * tied to a specific place on the document. Just if the user hits enter
-     * anywhere on the document, it should add if there is something to add.
-     * The function in this case takes one argument, which is they key 
-     */
-    document.addEventListener("keypress",function(event){
-        //Which is used by older browsers. Use an or statement for 
-        //compatability with other browsers.
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return{
+        init: function(){
+            console.log("APP HAS STARTED");
+            setupEventListeners();
         }
-
-    });
-
-
-
+    }
 
  })(budgetController,UIController);
 
 
-
+ //Calling this runs the app. Without this, app will not run.
+controller.init();
 
 
 
